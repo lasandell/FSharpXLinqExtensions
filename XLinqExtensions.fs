51ed23d07c31e286cec338fa4a141a10abe60d27
Luke Sandell <lasandell@gmail.com>
2015-02-08 09:30:22 -0600

    Use flexible types for XLinqSeqExtensions

--- XLinqExtensions.fs
+++ XLinqExtensions.fs
@@ -1,7 +1,6 @@
 ﻿[<AutoOpen>]
 module System.Xml.Linq.FSharpExtensions
 
-open System
 open System.Runtime.CompilerServices
 open System.Xml.Linq
 
@@ -25,12 +24,12 @@ type XElement with
 
 [<Extension>]
 type XLinqSeqExtensions =
-    [<Extension>] static member Ancestors(source:seq<XNode>, name) = source.Ancestors(XName.Get name)
+    [<Extension>] static member Ancestors(source:seq<#XNode>, name) = source.Ancestors(XName.Get name)
     [<Extension>] static member AncestorsAndSelf(source:seq<XElement>, name) = source.AncestorsAndSelf(XName.Get name)
     [<Extension>] static member Attributes(source:seq<XElement>, name) = source.Attributes(XName.Get name)
-    [<Extension>] static member Descendants(source:seq<XContainer>, name) = source.Descendants(XName.Get name)
+    [<Extension>] static member Descendants(source:seq<#XContainer>, name) = source.Descendants(XName.Get name)
     [<Extension>] static member DescendantsAndSelf(source:seq<XElement>, name) = source.DescendantsAndSelf(XName.Get name)
-    [<Extension>] static member Elements(source:seq<XContainer>, name) = source.Elements(XName.Get name)
+    [<Extension>] static member Elements(source:seq<#XContainer>, name) = source.Elements(XName.Get name)
 
 let XElement(name, content) = new XElement(XName.Get name, Seq.toArray content)
 let XAttribute(name, value) = new XAttribute(XName.Get name, value)
